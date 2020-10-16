4df02c57f178c6479bbd92e3a1cea86752af5d4b
KazeEternal <kyle.a.peplow@gmail.com>
2020-10-15 22:42:31 -0400

    [AppDomain] Converting to using a separate appdomain for loading/unloading the assembly. Still not working on unload. Crashes on current assembly load.

---
+++ Automata/DomainProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automata
+{
+    public class DomainProxy : MarshalByRefObject
+    {
+        private Assembly mScriptsAssembly = null;
+        public bool LoadDll(string path)
+        {
+            var bytes = System.IO.File.ReadAllBytes(path);
+            //mScriptsAssembly = System.Reflection.Assembly.Load(bytes);
+            mScriptsAssembly = Assembly.LoadFile(path);
+            return true;
+        }
+
+        public string GetStringTest()
+        {
+            Type myType = mScriptsAssembly.GetType("Test.TestClass");
+            // Get the method to call.
+            MethodInfo myMethod = myType.GetMethod("ReturnAString");
+
+             myMethod.Invoke(null, null);
+            return null;
+        }
+    }
+}
--- Automata/MainWindow.xaml.cs
+++ Automata/MainWindow.xaml.cs
@@ -1,22 +1,12 @@
 ﻿using Microsoft.CSharp;
 using System;
 using System.CodeDom.Compiler;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
+using System.Security.Policy;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Path = System.IO.Path;
 
 namespace Automata
@@ -24,6 +14,7 @@ namespace Automata
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    [Serializable]
     public partial class MainWindow : Window
     {
 
@@ -44,50 +35,31 @@ namespace Automata
             FileInfo fInfoPdb = new FileInfo("Scripts.pdb");
             if (fInfoDll.Exists)
             {
-                AppDomainSetup setup = AppDomain.CurrentDomain.SetupInformation;
-                setup.ApplicationBase = fInfoDll.FullName;
-                mScriptsDomain = AppDomain.CreateDomain("Scripts", null, setup);
+                try
+                {
+                    //fInfoDll.Delete();
+                    AppDomainSetup setup = new AppDomainSetup();
+                    setup.ApplicationBase = System.Environment.CurrentDirectory;
+                    Evidence evidence = AppDomain.CurrentDomain.Evidence;
+                    //setup.PrivateBinPath = Path.GetDirectoryName(fInfoDll.FullName);
+                    mScriptsDomain = AppDomain.CreateDomain("Automata Scripts Domain", evidence, setup);
 
-                //using (FileStream assemblyStream = new FileStream(fInfoDll.FullName, FileMode.Open))
-                //using (FileStream symbolsStream = new FileStream(fInfoPdb.FullName, FileMode.Open))
-                //{
-                //    byte[] bufferDll = new byte[assemblyStream.Length];
-                //    byte[] bufferPdb = new byte[symbolsStream.Length];
+                    Type proxyType = typeof(DomainProxy);
+                    DomainProxy proxy = (DomainProxy)mScriptsDomain.CreateInstanceFromAndUnwrap(proxyType.Assembly.FullName, proxyType.FullName);
 
-                //    assemblyStream.Read(bufferDll, 0, (int)assemblyStream.Length);
-                //    symbolsStream.Read(bufferPdb, 0, (int)assemblyStream.Length);
+                    proxy.LoadDll(fInfoDll.FullName);
+                    string retVal = proxy.GetStringTest();
 
-                //    mScriptsAssembly = mScriptsDomain.Load(bufferDll,bufferPdb);
-                //    //mScriptsAssembly = mScriptsDomain.Load(AssemblyName.GetAssemblyName(fInfo.FullName));
-                //    //mScriptsAssembly = Assembly.LoadFrom("Scripts.dll");
-
-                //    // Get the type to use.
-                //    Type myType = mScriptsAssembly.GetType("Test.TestClass");
-                //    // Get the method to call.
-                //    MethodInfo myMethod = myType.GetMethod("ReturnAString");
-                //    // Create an instance.
-                //    //object obj = Activator.CreateInstance(myType);
-                //    // Execute the method.
-                //    //myMethod.Invoke(obj, null);
-                //    string retVal = (string)myMethod.Invoke(null, null);
-                //    MessageBox.Show(retVal);
-                //} 
-
-                mScriptsAssembly = mScriptsDomain.Load(AssemblyName.GetAssemblyName(fInfoDll.FullName));
-                //mScriptsAssembly = Assembly.LoadFrom("Scripts.dll");
-
-                // Get the type to use.
-                Type myType = mScriptsAssembly.GetType("Test.TestClass");
-                // Get the method to call.
-                MethodInfo myMethod = myType.GetMethod("ReturnAString");
-                // Create an instance.
-                //object obj = Activator.CreateInstance(myType);
-                // Execute the method.
-                //myMethod.Invoke(obj, null);
-                string retVal = (string)myMethod.Invoke(null, null);
-                MessageBox.Show(retVal);
+                    MessageBox.Show(retVal);
+                }
+                catch(FileNotFoundException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                
             }
         }
+
 
         private void InitializeFileSystemWatcher()
         {
@@ -106,8 +78,8 @@ namespace Automata
 
             // Add event handlers.
             watcher.Changed += OnChanged;
-            watcher.Created += OnChanged;
-            watcher.Deleted += OnChanged;
+            //watcher.Created += OnChanged;
+            //watcher.Deleted += OnChanged;
             watcher.Renamed += OnRenamed;
 
             // Begin watching.
@@ -122,54 +94,56 @@ namespace Automata
             
         }
 
+        object mutex = new object();
         private void OnChanged(object sender, FileSystemEventArgs e)
         {
-            if(mScriptsAssembly != null)
+
+            lock (mutex)
             {
-                AppDomain.Unload(mScriptsDomain);
-                mScriptsAssembly = null;
-                mScriptsDomain = null;
-            }
-
-            Thread.Sleep(5000);
-            CSharpCodeProvider provider = new CSharpCodeProvider();
-            CompilerParameters parameters = new CompilerParameters();
-
-            //parameters.ReferencedAssemblies.Add("")
-            parameters.GenerateInMemory = true;
-
-            parameters.IncludeDebugInformation = true;
-
-            parameters.GenerateExecutable = false;
-
-            parameters.OutputAssembly = "Scripts.dll";
-
-            parameters.WarningLevel = 3;
-
-            parameters.TempFiles = new TempFileCollection(".", true);
-
-            DirectoryInfo dInfo = new DirectoryInfo(mWatchPath);
-            String[] paths = dInfo.GetFiles("*.cs", SearchOption.AllDirectories).Select(o => o.FullName).ToArray(); 
-            
-            CompilerResults result = provider.CompileAssemblyFromFile(parameters,  paths);
-
-            
-
-            var errors = result.Errors;
-            if (errors.HasErrors)
-            {
-                foreach(var error in errors)
+                if (mScriptsDomain != null)
                 {
-                    Console.Error.WriteLine(error);
+                    AppDomain.Unload(mScriptsDomain);
+                    mScriptsAssembly = null;
+                    mScriptsDomain = null;
+                    GC.Collect();
+                    
                 }
-            }
-            else
-            {
-                Type t = result.CompiledAssembly.GetType("Test.TestClass");
 
-                MethodInfo mInfo = t.GetMethod("ReturnAString");
-                object retVal = mInfo.Invoke(null, null);
-                MessageBox.Show(retVal.ToString());
+                Thread.Sleep(5000);
+                CSharpCodeProvider provider = new CSharpCodeProvider();
+                CompilerParameters parameters = new CompilerParameters();
+
+                //parameters.ReferencedAssemblies.Add("")
+                parameters.GenerateInMemory = false;
+
+                parameters.IncludeDebugInformation = true;
+
+                parameters.GenerateExecutable = false;
+
+                parameters.OutputAssembly = "Scripts.dll";
+
+                parameters.WarningLevel = 3;
+
+                //parameters.TempFiles = new TempFileCollection(".", true);
+
+                DirectoryInfo dInfo = new DirectoryInfo(mWatchPath);
+                String[] paths = dInfo.GetFiles("*.cs", SearchOption.AllDirectories).Select(o => o.FullName).ToArray();
+
+                CompilerResults result = provider.CompileAssemblyFromFile(parameters, paths);
+
+
+                var errors = result.Errors;
+                if (errors.HasErrors)
+                {
+                    foreach (var error in errors)
+                    {
+                        Console.Error.WriteLine(error);
+                    }
+                }
+                else
+                {
+                    InitializeAppDomain();
+                }
             }
         }
     }
